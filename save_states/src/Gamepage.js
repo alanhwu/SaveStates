188772a4ab8dd4b639d85472f1bedd692cadef6a
Jonah Paten <jpaten@ucla.edu>
2021-11-22 13:53:12 -0800

    Tried to setup React-Bootstrap

--- save_states/src/Gamepage.js
+++ save_states/src/Gamepage.js
@@ -3,13 +3,8 @@ import './Gamepage.css';
 import { Link } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-/*<link
-rel="stylesheet"
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-crossOrigin="anonymous"
-    />*/
+import Button from 'react-bootstrap/Button';
+
 
 
 function Gamepage() {
