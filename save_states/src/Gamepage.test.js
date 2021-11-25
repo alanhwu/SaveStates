b9a6898bac42e779da0b25dfd6564bd3abcfc74d
Jed Miguel <jedmig4@gmail.com>
2021-11-25 11:57:14 -0800

    Deleted random files, started working on homepage

--- save_states/src/Gamepage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Homepage from './Homepage';
-
-test('renders learn react link', () => {
-  render(<Homepage />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
