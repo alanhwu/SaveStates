6172c2a588eb7a5965af01307e2c0c628925ef1d
Jonah Paten <jpaten@ucla.edu>
2021-11-24 16:25:34 -0800

    Added empty Userpage

---
+++ save_states/src/Userpage.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './Userpage.css';
+import { Link } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
+
+function Userpage() {
+    return (
+        <div className="User">
+            <h1>USER PAGE</h1>
+        </div>
+    );
+}
+
+export default Userpage;
