22e47b88c58c84fc5dec6d1b27a14d31ec930c52
Jed Miguel <jedmig4@gmail.com>
2021-11-26 11:39:37 -0800

    Configured navigation bar

--- save_states/src/Gamepage.js
+++ save_states/src/Gamepage.js
@@ -1,4 +1,5 @@
 import portalImage from './images/portal.jpg';
+import logo from './images/logo.svg';
 import './Gamepage.css';
 import { Link } from "react-router-dom";
 
@@ -7,19 +8,51 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Navbar, Form, FormControl } from 'react-bootstrap'
+
 
 function Gamepage() {
-    let gameName = "Portal"
+    let gameName = "Portal" 
     return (
         <Container>
-            <div className="Gamepage">
-                <Link to="/"><Button className="Homepage">Homepage</Button></Link>
+        <Navbar bg="dark" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="/">SaveStates</Navbar.Brand>
+                <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search for Games"
+                    classname="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+                </Form>
+                <div>
+                    <Navbar.Text>VaultBoy101</Navbar.Text>
+                    <Button variant="light">Logout</Button>
+                </div>
+                
+            </Container>
+        </Navbar>
+            <div>
                 <Row>
-                    <Col xs={6} md={4}>
-                        <Image src={portalImage} thumbnail />
+                    <Col>
+                        <Image src={portalImage} style={{}}thumbnail/>
                     </Col>
+                    <Col>
+                        <Row>
+                            <Col style={{}}>Rating</Col>
+                            <Col style={{}}>Publisher</Col>
+                        </Row>
+                        <Row>Add to backlog button</Row>
+                        <Row>Add new playthrough</Row>
+                    </Col>
+                    <Col>Table here</Col>
                 </Row>
+                <Row>Stuff here</Row>
             </div>
+            // Get a big form to write reviews in
+            //Accordion(maybe?) for user review
         </Container>
     );
 }
--- save_states/src/index.js
+++ save_states/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import Homepage from './Homepage';
