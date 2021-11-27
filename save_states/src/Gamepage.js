bd7963ec4f70478d54961ba2d13aa34c41871b6e
Jonah Paten <jpaten@ucla.edu>
2021-11-27 00:20:23 -0800

    Fixed really silly error in the prior commit

--- save_states/src/Gamepage.js
+++ save_states/src/Gamepage.js
@@ -20,29 +20,6 @@ function Gamepage() {
         <Container>
             {/*Navigation bar at the top, contains link to the homepage, search bar and logout button*/}
             {saveStatesNavbar((userName))}
-        <Navbar bg="dark" variant="dark">
-            <Container fluid>
-                {/*Links to homescreen*/}
-                <Navbar.Brand href="/">SaveStates</Navbar.Brand>
-                {/*TODO: implement querying here when the enter key is pressed*/}
-                <Form className="d-flex">
-                <FormControl
-                    type="search"
-                    placeholder="Search for Games"
-                    classname="me-2"
-                    aria-label="Search"
-                />
-                {/*TODO: figure out how to make searching work through the button*/}
-                <Button variant="outline-success">Search</Button>
-                </Form>
-                <div>
-                    {/*Username for the user*/}
-                    <Navbar.Text>{userName}</Navbar.Text>
-                    {/*Logout button. TODO: need to implement logging out when this button is pressed */}
-                    <Button variant="light">Logout</Button>
-                </div>
-            </Container>
-        </Navbar>
             {/*Header for the name*/}
             <div class = 'Gamepage-header'>{gameName}</div>
             <div>
