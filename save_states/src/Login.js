e4ae8476e5559d1d3e309d1d7bdc2b13ba941eef
Jonah Paten <jpaten@ucla.edu>
2021-12-01 12:16:20 -0800

    Made login page prettier with smaller textboxes

--- save_states/src/Login.js
+++ save_states/src/Login.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form'
 import SaveStatesNavbar from  "./saveStatesNavbar"
-import { FormControl, InputGroup } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 function Login() {
     
@@ -25,50 +25,67 @@ function Login() {
     }
 
     return (
-        <div class={"mx-5"}>
+        <div class={"mx-4"}>
             <div class="Login-page">
                 {SaveStatesNavbar()}
             <div>
-                <div className="Login-Header">Log in!</div>
-                <Form.Group className="Login-username mt-4" controlId="formBasicUsername">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="username" placeholder="Enter username" onChange={onUsernameChange} />
-                </Form.Group>
+                <Row>
+                    <div className="Login-Header">Enter the Dungeon</div>
+                    <Col />
+                    <Col md={4}>
+                        <Form.Group className="Login-username mt-3" controlId="formBasicUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="username" placeholder="Enter username" onChange={onUsernameChange} />
+                        </Form.Group>
+                    </Col>
+                    <Col />
+                </Row>
             </div>
 
             <br />
             <div>
-                <Form.Group className="Login-password" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" onChange={onPasswordChange} />
-                </Form.Group>
+                <Row>
+                    <Col />
+                    <Col md={4}>
+                        <Form.Group className="Login-password" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter password" onChange={onPasswordChange} />
+                        </Form.Group>
+                    </Col>
+                    <Col />
+                </Row>
             </div>
 
             <br />
-
-            <Button className="Login-button" onClick={() => {
-                // Grab the current value of the input field
-                const options = {
-                    method: 'GET'
-                }
-
-                // Redirect to your page after you log in
-                fetch('http://localhost:3001/finduser/' + username)
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.length === 0 || data[0].password != inputPassword) {
-                            alert("Username or password is incorrect! Please try again.");
-                            return null;
+            <Row>
+                <Col />
+                <Col className={"Login-button"}>
+                    <Button onClick={() => {
+                        // Grab the current value of the input field
+                        const options = {
+                            method: 'GET'
                         }
-                        localStorage.setItem("user", username);
-                        return data;
-                    })
-                    .then(data => {
-                        if (data != null) {
-                            window.location.href="/user?" + data[0].username;
-                        }
-                    })
-            }}>Log In</Button>
+
+                        // Redirect to your page after you log in
+                        fetch('http://localhost:3001/finduser/' + username)
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data.length === 0 || data[0].password != inputPassword) {
+                                    alert("Username or password is incorrect! Please try again.");
+                                    return null;
+                                }
+                                localStorage.setItem("user", username);
+                                return data;
+                            })
+                            .then(data => {
+                                if (data != null) {
+                                    window.location.href="/user?" + data[0].username;
+                                }
+                            })
+                    }}>Log In</Button>
+                </Col>
+                <Col />
+            </Row>
                 {/* TODO: For now this takes you to the user page but has no effect on the log in status,
                 make it so that it actually logs you in to your own page */}
             </div>
