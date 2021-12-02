98f07a5eea0da27be956d40488ad336bbe902552
Jonah Paten <jpaten@ucla.edu>
2021-12-01 16:46:07 -0800

    Added navbar to error pages

--- save_states/src/NoGameError.js
+++ save_states/src/NoGameError.js
@@ -2,7 +2,7 @@ import React, {Component, useState} from 'react';
 import { Link } from "react-router-dom";
 
 import {useParams} from "react-router-dom";
-import saveStatesNavbar from "./saveStatesNavbar";
+import SaveStatesNavbar from "./saveStatesNavbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {Modal, ListGroup, Table, Form} from "react-bootstrap";
@@ -16,7 +16,7 @@ export default function GameError(){
 
     return (
         <Container>
-            <Link to = "/"><h1 className="">SaveStates</h1></Link> 
+            {SaveStatesNavbar()}
             {/*Header for the name*/}
             <div class = 'Error-header'><h1>Error</h1></div>
             <div class = 'Error-message'><h1>Game Over</h1></div>
--- save_states/src/NoUserError.js
+++ save_states/src/NoUserError.js
@@ -2,7 +2,7 @@ import React, {Component, useState} from 'react';
 import { Link } from "react-router-dom";
 
 import {useParams} from "react-router-dom";
-import saveStatesNavbar from "./saveStatesNavbar";
+import SaveStatesNavbar from "./saveStatesNavbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {Modal, ListGroup, Table, Form} from "react-bootstrap";
@@ -16,7 +16,7 @@ export default function UserError(){
 
     return (
         <Container>
-            <Link to = "/"><h1 className="">SaveStates</h1></Link> 
+            {SaveStatesNavbar()}
             {/*Navigation bar at the top, contains link to the homepage, search bar and logout button*/}
             {/*Header for the name*/}
             <div class = 'Error-header'><h1>Error</h1></div>
